78805eb5566a6162b5ac93aa890a4f1be8a45d8e
fmatter <florianmatter@gmail.com>
2021-09-28 20:16:21 +0200

    working on overview of etxensions and verbs

--- workflow/irregular_paradigms.py
+++ workflow/irregular_paradigms.py
@@ -1,7 +1,9 @@
 import pyradigms as pyd
 import pandas as pd
-        
+
+cs_df = pd.read_csv("../data/cognate_sets.csv")
 df = pd.read_csv("../data/irregular_verb_data.csv")
+print(cs_df)
 
 lg_list = ["PWai", "wai", "hix", "PPek", "PXin", "ara", "ikp", "bak", "PTir", "aku", "tri", "car", "yuk"]
 # for i in ["PWai", "PPek", "PXin", "PTir"]:
@@ -11,10 +13,21 @@ pyd.x = ["Cognateset_ID"]
 pyd.y = ["Language_ID"]
 pyd.z = []
 verb_list = ["come-1", "go", "say", "go.down", "be-1", "be-2"]
-pyd.x_sort = lg_list
+pyd.x_sort = verb_list
+pyd.y_sort = lg_list
 pyd.filters = {"Inflection": ["1"], "Cognateset_ID": verb_list, "Language_ID": lg_list}
-# pyd.content_string = "Prefix_Cognateset_ID"
+pyd.content_string = "Prefix_Cognateset_ID"
 table = pyd.compose_paradigm(df)
+table.replace("", "–", inplace=True)
+
+def identify_unaffected(value):
+    if ("1w" in value or "1p" in value):
+        return "y"
+    elif value in ["?", "–"]:
+        return value
+    else:
+        return "n"
+table = table.applymap(identify_unaffected)
 print(table)
 #
 # for verb in verb_list:
