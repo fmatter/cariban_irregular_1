f1bc3dd7893e7375dde3088753f24abc73031fc7
fmatter <florianmatter@gmail.com>
2021-10-25 18:38:34 +0200

    various improvements on the CLDF dataset

--- workflow/compile_cldf_irregular.py
+++ workflow/compile_cldf_irregular.py
@@ -15,6 +15,7 @@ cogsets = pd.read_csv("../data/cognate_sets.csv")
 examples = pd.read_csv("../data/examples.csv")
 v_forms = v_forms[v_forms["Inflection"].isin(["1"])]
 v_forms.reset_index(inplace=True, drop=True)
+lex_df = pd.read_csv("../data/other_lexemes.csv")
 
 print("Compiling meanings")
 bathe["Meaning"] = bathe["Transitivity"].apply(
@@ -70,8 +71,14 @@ form_meanings.rename(columns={"Parameter_ID": "ID"}, inplace=True)
 form_meanings.drop(columns=["Inflection", "Meaning"], inplace=True)
 form_meanings.drop_duplicates(subset=["ID"], inplace=True)
 
+lex_meanings = lex_df[["Meaning"]]
+lex_meanings["Name"] = lex_meanings["Meaning"].str.replace(" ", ".")
+lex_meanings["Description"] = "'" + lex_meanings["Meaning"] + "'"
+lex_meanings["ID"] = lex_meanings["Meaning"].str.replace(" ", "_")
+lex_meanings.drop(columns=["Meaning"], inplace=True)
+lex_meanings.drop_duplicates(subset=["ID"], inplace=True)
 
-meanings = verb_meanings.append(form_meanings)
+meanings = verb_meanings.append(form_meanings).append(lex_meanings)
 
 print("Processing examples")
 # prepare examples
@@ -119,8 +126,10 @@ def split_cogset_ids(
 ):
     out = []
     for i, row in df[[id, cogset_col]].iterrows():
+        if pd.isnull(row[cogset_col]): continue
         if sep in row[cogset_col]:
             for j, cog in enumerate(row[cogset_col].split(sep)):
+                if cog == "?": continue
                 out.append(
                     {
                         "ID": row[id] + "_" + bare + "_" + suffixes[j],
@@ -130,6 +139,7 @@ def split_cogset_ids(
                     }
                 )
         else:
+            if row[cogset_col] == "?": continue
             out.append(
                 {
                     "ID": row[id] + "_" + bare,
@@ -158,7 +168,7 @@ cogsets["Description"] = (
     + cogsets["Form"].astype(str)
     + " '"
     + cogsets["Meaning"].astype(str)
-    + "' "
+    + "' . "
     + cogsets["Comment"].astype(str)
 )
 cogsets.drop(columns=["Form", "Meaning", "Comment"], inplace=True)
@@ -177,6 +187,14 @@ form_cogs2 = split_cogset_ids(
     v_forms, cogset_col="Verb_Cognateset_ID", suffixes=["pfx", "root"], bare="stem"
 )
 
+lex_df.rename(columns={"Meaning": "Parameter_ID"}, inplace=True)
+lex_df["Parameter_ID"] = lex_df["Parameter_ID"].str.replace(" ", "_")
+lex_df.index = lex_df["Language_ID"] + "_other_" + lex_df.index.astype(str)
+lex_df.index.name = "ID"
+lex_df.reset_index(inplace=True)
+lex_df["Segments"] = lex_df["Form"].map(segmentify)
+lex_df["Comment"] = "Form in source: " + lex_df["Full_Form"] + "."
+lex_df.drop(columns=["Full_Form"], inplace=True)
 
 def fix_cog_count(row, prev_df):
     others = prev_df[prev_df["Form_ID"] == row["Form_ID"]]
@@ -197,6 +215,10 @@ form_cogs2["Segment_Slice"] = form_cogs2.apply(
 v_forms["Cognateset_ID"] = (
     v_forms["Prefix_Cognateset_ID"] + "+" + v_forms["Verb_Cognateset_ID"]
 )
+v_forms["Full_Form"] = "Form in source: " + v_forms["Full_Form"] + "."
+
+v_forms["Comment"] = v_forms[["Comment", "Full_Form"]].values.tolist()
+v_forms["Comment"] = v_forms.apply(lambda row: " ".join([x for x in row["Comment"] if not pd.isnull(x)]), axis=1)
 v_forms.drop(
     columns=["Verb_Cognateset_ID", "Prefix_Cognateset_ID", "Full_Form", "Inflection"],
     inplace=True,
@@ -207,7 +229,7 @@ cognates = verb_cogs.append(form_cogs1).append(form_cogs2)
 cognates.sort_values(by=["Form_ID", "Segment_Slice"], inplace=True)
 cognates.reset_index(inplace=True, drop=True)
 
-forms = v_forms.append(verbs)
+forms = v_forms.append(verbs).append(lex_df)
 forms["Form"] = forms["Form"].str.replace("+", "", regex=False)
 forms.drop(columns=["Cognateset_ID", "Meaning"], inplace=True)
 
